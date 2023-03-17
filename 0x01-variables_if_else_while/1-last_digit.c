4f39e8d05022c077ed8917b2f0bfeb41e6c10d2f
khabure <jmkhabure@gmail.com>
2023-03-17 15:24:04 -0700

    Fix

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -22,8 +22,8 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, last);
 	} else if (last == 0)
 	{
-		printf("Last digit of %d is 0\n", n);
-	} else if (last < 6)
+		printf("Last digit of %d is 0 and is 0\n", n);
+	} else if (last < 6)i
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last);
 	}
