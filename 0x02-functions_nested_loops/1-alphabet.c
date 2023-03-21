fa502f052e03ff34a352db7df7cc92e435622380
khabure <jmkhabure@gmail.com>
2023-03-21 10:32:06 -0700

    Fixes

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
  * Description: 'the program's description'
  *
  * Return: Always 0 (Success)
  */
 
-int main(void)
+int print_alphabet(void)
 {
-        char str[] = "abcdefghijklmnopqrstuvwxyz\n";
-        int n;
+	char str[] = "abcdefghijklmnopqrstuvwxyz\n";
+	int n;
 
-        for (n = 0; n < 9; n++)
-        {
-        _putchar(str[n]);
-        }
-        return (0);
+	for (n = 0; n < 9; n++)
+	{
+		_putchar(str[n]);
+	}
+	return (0);
 }
