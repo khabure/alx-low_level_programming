94f726e9d31c6d910e4d29a16eccdce064e57de1
khabure <jmkhabure@gmail.com>
2023-03-21 13:23:49 -0700

    Added times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * times_table - checks if character is in lower case
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 1 or 0 (Success)
+ */
+
+void times_table(void)
+{
+	int a;
+	int b;
+	int ans;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			ans = a * b
+			_putchar((ans / 10) + '0');
+			_putchar((i % 10) + '0');
+			_putchar(44);
+			_putchar(32);
+		}
+		_putchar('\n');
+	}
+}
