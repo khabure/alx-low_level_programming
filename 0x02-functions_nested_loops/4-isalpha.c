205b14f4401d4c1cff1dcd0470fb505e9cc239a0
khabure <jmkhabure@gmail.com>
2023-03-21 11:22:10 -0700

    Added 4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if character is alphanumeric
+ *
+ * Description: 'the program's description'
+ *
+ * @c: the inputed character
+ * Return: Always 1 or 0 (Success)
+ */
+
+int _isalpha(int c)
+{
+        if (c >= 65 && c <= 90)
+        {
+                return (1);
+        } else if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+        return (0);
+}
