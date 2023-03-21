d10bb54879dd98bfd25bf7ba56222dc2483f3bf2
khabure <jmkhabure@gmail.com>
2023-03-21 13:47:59 -0700

    fix

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add  - checks if character is in lower case
+ *
+ * Description: 'the program's description'
+ *
+ * @n: the inputed character
+ * @m: number
+ * Return: Always 1 or 0 (Success)
+ */
+
+int add(int n, int m)
+{
+	int sum = n + m;
+
+	return (sum);
+}
