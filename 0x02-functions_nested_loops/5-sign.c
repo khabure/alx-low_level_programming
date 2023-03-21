bbee985f00855a55e5b7df4c87cf60bfae229d31
khabure <jmkhabure@gmail.com>
2023-03-21 11:29:47 -0700

    Added 5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_sign - print sign of a number
+ *
+ * Description: 'the program's description'
+ *
+ * @n: the inputed character
+ * Return: Always a string (Success)
+ */
+
+char print_sign(int n)
+{
+	if (n > 0)
+	{
+		return ("+");
+	} else if (n < 0)
+	{
+		return ("-");
+	}
+	return ("0");
+}
