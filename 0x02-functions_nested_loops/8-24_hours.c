04e82a0933f5e9b18815f300087c8f8256d4e198
khabure <jmkhabure@gmail.com>
2023-03-21 12:57:07 -0700

    Fix

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -19,7 +19,7 @@ void jack_bauer(void)
 	{
 		for (b = 0; b <= 9 && (10 * a + b) <= 24; b++)
 		{
-			for (c = 0; c <= 6; c++)
+			for (c = 0; c <= 6 && (10 * c + d) <= 60; c++)
 			{
 				for (d = 0; d <= 9; d++)
 				{
