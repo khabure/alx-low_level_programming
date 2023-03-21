58933dfcc29ba65afb1ea72042e60abd27ae9a18
khabure <jmkhabure@gmail.com>
2023-03-21 10:27:25 -0700

    Added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char str[] = "abcdefghijklmnopqrstuvwxyz\n";
+        int n;
+
+        for (n = 0; n < 9; n++)
+        {
+        _putchar(str[n]);
+        }
+        return (0);
+}
