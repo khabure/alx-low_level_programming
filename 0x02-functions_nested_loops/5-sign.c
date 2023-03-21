09e5c10ad3464aae4c9215bde557d22c5cdea56c
khabure <jmkhabure@gmail.com>
2023-03-21 11:42:17 -0700

    Fix

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,13 +13,13 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar("+");
+		_putchar('+');
 		return (0);
 	} else if (n < 0)
 	{
-		_putchar("-");
+		_putchar('-');
 		return (0);
 	}
-	_putchar("0");
+	_putchar('0');
 	return (0);
 }
