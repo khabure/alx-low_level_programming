e4c6b3a94e9223c63b0bda877d4fd95157e458a5
khabure <jmkhabure@gmail.com>
2023-03-21 11:49:27 -0700

    Added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _abs - checks if character is in lower case
+ *
+ * Description: 'the program's description'
+ *
+ * @n: the inputed character
+ * Return: Always 1 or 0 (Success)
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+}
