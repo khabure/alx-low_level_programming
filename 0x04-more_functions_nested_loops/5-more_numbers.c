a46bfed56bc3f75bb94313a74c9961ba51473982
khabure <jmkhabure@gmail.com>
2023-03-23 10:36:39 -0700

    Fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,7 +8,10 @@
 void more_numbers(void)
 {
 	int i;
+	int j;
 
+	for (j = 0; j < 10; j++)
+	{
 	for (i = 0; i <= 14; i++)
 	{
 		if (i >= 10)
@@ -19,4 +22,5 @@ void more_numbers(void)
 
 	}
 	_putchar('\n');
+	}
 }
