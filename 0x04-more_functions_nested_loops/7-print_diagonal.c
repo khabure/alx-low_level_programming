7ad52831301ee25c193bdfe0c2985f15927b5e2c
khabure <jmkhabure@gmail.com>
2023-03-23 11:02:22 -0700

    Added diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draw a diagonal
+ * @n: number of \ printed
+ * Return: Always 0.
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(92);
+		_putchar('\n');
+		int j;
+
+		for (j = 0; j < i; j++)
+		{
+			_putchar(32);
+		}
+	}
+}
