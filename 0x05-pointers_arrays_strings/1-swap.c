3b4ca28570a6458910a7c49de3cd1165d5120da0
khabure <jmkhabure@gmail.com>
2023-03-27 07:49:05 -0700

    Added swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - swap the integers
+ * @a: first int
+ * @b: second int
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = a;
+	a = b;
+	b = c;
+}
