bb0e8a9edf9e9626e63cb70cb4cbb690e7db154a
khabure <jmkhabure@gmail.com>
2023-04-10 12:26:23 -0700

    Added 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - main code
+ *
+ * @argc: The number of commands
+ * @argv: the list of arguments
+ * Return: Always 0
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf(argv[0]);
+}
